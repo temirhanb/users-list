87242453fd49465d5a69c44df4ee6e422dfc4957
temirhanb <temirhan023@gmail.com>
2025-10-02 00:26:05 +0500

    - fix schema length

--- src/shared/schema/index.ts
+++ src/shared/schema/index.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 export const SCHEMA_FORM = z.object({
-  name: z.string().min(1, "Name is required"),
+  name: z.string().min(2, "Minimum length: 2"),
   email: z.string().email("Format mail@mail.com"),
   phone: z.string().min(10, "Format +7 (999) 999 99 99"),
   role: z.string().min(1, "Role is required")
