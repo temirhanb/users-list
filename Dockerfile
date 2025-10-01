97f6c619795738e34e66bf39da87f67d5029724e
temirhanb <temirhan023@gmail.com>
2025-10-02 00:23:30 +0500

    - add docker script

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,17 @@
-FROM node:lts as dependencies
-WORKDIR /users-list
-COPY package.json package-lock.json ./
-RUN npm install --frozen-lockfile
+FROM node:20-alpine
 
-FROM node:lts as builder
 WORKDIR /users-list
+
+COPY package.json .
+
+RUN npm install
+
+RUN npm i -g serve
+
 COPY . .
-COPY --from=dependencies /users-list/node_modules ./node_modules
+
 RUN npm run build
 
-FROM node:lts as runner
-WORKDIR /users-list
-ENV NODE_ENV production
-
-COPY --from=builder /users-list/public ./public
-COPY --from=builder /users-list/package.json ./package.json
-COPY --from=builder /users-list/node_modules ./node_modules
-
 EXPOSE 3000
-CMD ["npm", "start"]
+
+CMD [ "serve", "-s", "dist" ]
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path';
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, './src/app'), // Example alias for components
-      '@pages': path.resolve(__dirname, './src/pages'), // Example alias for components
-      '@shared': path.resolve(__dirname, './src/shared'), // Example alias for components
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"), // Example alias for components
+      "@pages": path.resolve(__dirname, "./src/pages"), // Example alias for components
+      "@shared": path.resolve(__dirname, "./src/shared"), // Example alias for components
     },
   },
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
